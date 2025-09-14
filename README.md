94280c5cb891f7ac81d944f01df142b888ed2f2f
reskoldo73 <ldecarlos73@gmail.com>
2025-09-14 23:16:13 +0200

    Added backtracking folders and the first problem.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # algorismia-fme-2025
-Problems from the 2025 Course on Algorithms from the FME-UPC
+Problems from the 2025 Course on Algorithms from the FME-UPC.
+These are just my messy codes and I'm not a professional, I just want to practice using git and github.
---
+++ problems/1-backtracking/1.1-subsets/P12828-Zeros-and-Ones-1.cc
@@ -0,0 +1,44 @@
+
+/*
+Write a backtracking program to print all the combinations of z zeros and o ones such that z + o = n, for a given n.
+
+Input
+
+Input consists of a natural number n > 0.
+
+Output
+
+Print all the combinations of z zeros and o ones such that z + o = n, one per line and in lexicographical order.
+
+Observation
+
+Although a backtracking program is not really necessary to solve this exercise, implement it anyway for the sake of practice.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+using vb = vector<bool>;
+
+int n;
+vb v;
+
+void print() {
+    for(int i = 0; i < n; ++i) cout << (i ? " " : "") << v[i];
+    cout << "\n";
+}
+
+void solve(int pos) {
+    if(pos == n) return print();
+    v[pos] = 0;
+    solve(pos+1);
+    v[pos] = 1;
+    solve(pos+1);
+}
+
+int main() {
+    cin >> n;
+    v = vb(n);
+    solve(0);
+}
